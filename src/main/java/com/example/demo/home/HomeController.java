f75ce5feb8f8539896343cc37781830b5fcb91bd
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-05-10 22:04:11 -0300

    updating files

--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"http://localhost:4200","https://makemetechieapi.herokuapp.com"})
+@CrossOrigin(origins = {"http://localhost:4200","https://makemetechie.herokuapp.com"})
 @RestController
 public class HomeController {
 
